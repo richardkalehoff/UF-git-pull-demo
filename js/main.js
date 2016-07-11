036eef423e4db37f95e58a07b37022d6fec139bf
Richard Kalehoff <richardkalehoff@gmail.com>
2016-07-10 17:46:31 -0700

    add message to console

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var styles = [
+    'background: linear-gradient(90deg, #ff9900, #0008ff)',
+    'border: 1px solid #001dff',
+    'color: white',
+    'display: block',
+    'text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3)',
+    'line-height: 40px',
+    'text-align: center',
+    'font-size: 1.2em',
+    'font-weight: bold',
+    'text-transform: uppercase',
+    'padding: 1em 2.5em',
+    'margin-top: 1em'
+].join(';');
+
+console.log("Git's %cpull%c is awesome", styles, '');
+console.log('Check out the docs here https://git-scm.com/docs/git-pull');
